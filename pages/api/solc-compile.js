4af0dffbd7d285beef21dfdc94779938f85e8c92
pratiqdev <pratiqdev@gmail.com>
2022-05-29 11:35:49 -0400

    testing code compiler api

--- pages/api/solc-compile.js
+++ pages/api/solc-compile.js
@@ -7,7 +7,7 @@ import { code } from '../../data/node-memory';
 export default async function handler(req, res) {
   return new Promise((resolve)=>{
 
-
+    console.log('/api/solc-compile | starting compilation...')
   
   let USE_VERSION
   let START_TIMESTAMP = Date.now()
@@ -15,11 +15,15 @@ export default async function handler(req, res) {
   try{
     
     const data = requireFromUrl('https://binaries.soliditylang.org/bin/list.js');
+    console.log('/api/solc-compile | releases length:', data.releases.length)
+
     const AVAILABLE_VERSIONS = data.releases
     const givenCode = req.body.code
     let givenVersion = req.body.version
 
     if(!givenCode){
+      console.log('/api/solc-compile | no code in body')
+
       res.status(500).json('Must supply code to compile. Recieved:' + req.body)
       resolve()
       return
@@ -39,6 +43,8 @@ export default async function handler(req, res) {
     
     const loadSolc = async (_version) => {
       return new Promise((res) => {
+        console.log('/api/solc-compile | loadSolc...')
+
 
           if(!_version){
               let version = solc.version() 
@@ -47,7 +53,7 @@ export default async function handler(req, res) {
                   solc
               })
           }
-          console.log(`>> loadSolc | loading version: ${_version}`)
+          console.log(`/api/solc-compile | loadSolc | loading version: ${_version}`)
 
           const cleanVersion = _version
               .replace('soljson-', '')
@@ -55,7 +61,7 @@ export default async function handler(req, res) {
 
           solc.loadRemoteVersion(cleanVersion, async (err, solcSnapshot) => {
             if (err) {
-                  console.log(`> loadSolc | could not load version: ${cleanVersion}`)
+                  console.log(`/api/solc-compile | loadSolc | could not load version: ${cleanVersion}`)
                   let version = solc.version()
                   res({
                       version,
@@ -63,7 +69,7 @@ export default async function handler(req, res) {
                   })
               }else{
 
-                  console.log(`> loadSolc | loaded version: ${cleanVersion}`)
+                  console.log(`/api/solc-compile | loadSolc | loaded version: ${cleanVersion}`)
                   
                   let version = solcSnapshot.version()
                   res({
@@ -76,6 +82,8 @@ export default async function handler(req, res) {
     }
 
     const compileSource = async (solc, data) => {
+      console.log(`/api/solc-compile | compileSource...`)
+      
         const input = {
             language: 'Solidity',
             sources: {
@@ -96,7 +104,7 @@ export default async function handler(req, res) {
         let result = await solc.compile(inputString)
         // let output = JSON.parse(result)
 
-        console.log('>> done compiling!')
+        console.log('/api/solc-compile | compileSource | done compiling!')
         return { 
           success: true, 
           output: result,  
@@ -109,6 +117,7 @@ export default async function handler(req, res) {
 
     // console.log(typeof givenCode)
     if(givenCode.includes('pragma solidity')){
+      console.log(`/api/solc-compile | pragma found. parsing...`)
       let codeLines = givenCode.split('\n')
       let specVersion = null
       for(let i = 0; i<codeLines.length;i++){
@@ -120,24 +129,28 @@ export default async function handler(req, res) {
           }
           continue;
         }
-      console.log(`>> Code specified version: ${specVersion}`,)
+      console.log(`/api/solc-compile | code specified version: ${specVersion}`,)
       if(specVersion && specVersion in AVAILABLE_VERSIONS){
         USE_VERSION = AVAILABLE_VERSIONS[specVersion]
-        console.log(`>> Using specified version ${specVersion}: ${AVAILABLE_VERSIONS[specVersion]}`)
+        console.log(`/api/solc-compile | Using specified version ${specVersion}: ${AVAILABLE_VERSIONS[specVersion]}`)
       }else{
+        console.log(`/api/solc-compile | spec version not found. using default`)
         USE_VERSION = Object.values(AVAILABLE_VERSIONS)[0]
       }
     }
 
     else{
+      console.log(`/api/solc-compile | no pragma used`)
       USE_VERSION = Object.values(AVAILABLE_VERSIONS)[0]
     }
 
     
     loadSolc(USE_VERSION)
     .then(x => {
+      console.log(`/api/solc-compile | solc loaded...`)
       compileSource(x.solc, givenCode)
       .then(z => {
+        console.log(`/api/solc-compile | code compiled...`)
         res.status(200).json(z)
         resolve()
       })
@@ -145,7 +158,7 @@ export default async function handler(req, res) {
     
   
   }catch(err){
-    console.log('>> compiler error! ------------------------------------------')
+    console.log('/api/solc-compile | ERROR ------------------------------------------')
     console.log(err)
     res.status(200).json({ 
       success:false, 
